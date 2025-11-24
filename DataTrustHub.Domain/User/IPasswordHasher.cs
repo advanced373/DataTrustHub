f94ef2eec867c45c1c513587798ce3be4e3d72fe
advanced373 <mihaistoica1998@gmail.com>
2025-11-24 22:33:46 +0200

    #12 Add password hasher.

---
+++ DataTrustHub.Domain/User/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DataTrustHub.Domain.User
+{
+    public interface IPasswordHasher
+    {
+        public string HashPassword(string password);
+        public bool VerifyHashedPassword(string hashedPassword, string providedPassword);
+    }
+}
